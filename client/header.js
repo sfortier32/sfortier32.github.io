078d176c0ceb2cfee986bcc3de9185b97b108cd5
Sophia Fortier <sopfor02@me.com>
2023-10-12 21:50:31 -0400

    Pages
    - starting stylization
    - separating stylesheets

--- client/header.js
+++ client/header.js
@@ -5,9 +5,11 @@ function loadHeader() {
         <a href="../index.html">
             <div class="nav-name" id="name">Sophia Fortier</div>
         </a>
-        <div><a href="https://github.com/sfortier32" target="_blank" class="fa fa-2x fa-github icon"></a></div>
-        <div><a href="https://linkedin.com/in/sophiagfortier" target="_blank" class="fa fa-2x fa-linkedin icon"></a></div>
-        <div><a href="#email" target="_blank" class="fa fa-2x fa-envelope icon"></a></div>
+        <div class="icons">
+            <div><a href="https://github.com/sfortier32" target="_blank" class="fa fa-2x fa-github icon"></a></div>
+            <div><a href="https://linkedin.com/in/sophiagfortier" target="_blank" class="fa fa-2x fa-linkedin icon"></a></div>
+            <div><a href="#email" target="_blank" class="fa fa-2x fa-envelope icon"></a></div>
+        </div>
     `;
 };
 window.addEventListener("load", loadHeader());
