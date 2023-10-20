b6c64863ef71b0dbd48a7830c6541a3c06c0a18d
Sophia Fortier <sopfor02@me.com>
2023-10-20 12:04:01 -0400

    Email functionality

--- client/header.js
+++ client/header.js
@@ -8,7 +8,7 @@ function loadHeader() {
         <div class="icons">
             <div><a href="https://github.com/sfortier32" target="_blank" class="fa fa-2x fa-github icon"></a></div>
             <div><a href="https://linkedin.com/in/sophiagfortier" target="_blank" class="fa fa-2x fa-linkedin icon"></a></div>
-            <div><a href="#email" target="_blank" class="fa fa-2x fa-envelope icon"></a></div>
+            <div><a href="mailto:sopfor02@me.com" target="_blank" class="fa fa-2x fa-envelope icon"></a></div>
         </div>
     `;
 };
