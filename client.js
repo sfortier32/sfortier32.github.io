cc0eb35c773f1ab130e3f0f390d6f400c49d7451
Sophia Fortier <sopfor02@me.com>
2023-10-23 16:34:58 -0400

    Habit Tracker Page
    - Also removed header.js

--- client.js
+++ client.js
@@ -14,10 +14,36 @@ function stickyHeader() {
     }
 };
 
-
 // scroll to top
 const scrollback = document.getElementById('scrollback');
 scrollback.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' })
+});
+
+function loadFooter() {
+    scrollback.classList.add('stack');
+    scrollback.innerHTML = `
+        <img style="height: 20px;" src="https://live.staticflickr.com/65535/53280889465_fd6314f3d7_o.png">
+        <div class="footer-text">Go Back Up</div>
+    `;
+}
+
+function loadHeader() {
+    const navbar = document.getElementById('nav-bar');
+    navbar.innerHTML = `
+        <a href="../index.html">
+            <div class="nav-name" id="name">Sophia Fortier</div>
+        </a>
+        <div class="icons">
+            <div><a href="https://github.com/sfortier32" target="_blank" class="fa fa-2x fa-github icon"></a></div>
+            <div><a href="https://linkedin.com/in/sophiagfortier" target="_blank" class="fa fa-2x fa-linkedin icon"></a></div>
+            <div><a href="mailto:sopfor02@me.com" target="_blank" class="fa fa-2x fa-envelope icon"></a></div>
+        </div>
+    `;
+};
+
+window.addEventListener("load", (e) => {
+    loadHeader();
+    loadFooter();
 });
